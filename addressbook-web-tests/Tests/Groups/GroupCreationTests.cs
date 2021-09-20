ba8b16681d9144bbfaac76709589cee31ad217d7
Boris <lbomarleyl@gmail.com>
2021-09-20 17:05:21 +0300

    add test data generation to file

--- addressbook-web-tests/Tests/Groups/GroupCreationTests.cs
+++ addressbook-web-tests/Tests/Groups/GroupCreationTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading;
@@ -10,21 +11,38 @@ namespace WebAddressBookTests
     [TestFixture]
     public class GroupCreationTests : AuthTestBase
     {
-        public static IEnumerable<GroupData> RandomGroupDataProvider()
+        //public static IEnumerable<GroupData> RandomGroupDataProvider()
+        //{
+        //    List<GroupData> groups = new List<GroupData>();
+        //    for (int i = 0; i < 5; i++)
+        //    {
+        //        groups.Add(new GroupData(GenerateRandomString(30))
+        //        {
+        //            Header = GenerateRandomString(100),
+        //            Footer = GenerateRandomString(100)
+        //        });
+        //    }
+        //    return groups;
+        //}
+
+        public static IEnumerable<GroupData> GroupDataFromFile()
         {
             List<GroupData> groups = new List<GroupData>();
-            for (int i = 0; i < 5; i++)
+            string[] lines = File.ReadAllLines(@"groups.csv");
+            foreach (string l in lines)
             {
-                groups.Add(new GroupData(GenerateRandomString(30))
+                string[] parts = l.Split(',');
+                groups.Add(new GroupData(parts[0])
                 {
-                    Header = GenerateRandomString(100),
-                    Footer = GenerateRandomString(100)
+                    Header = parts[1],
+                    Footer = parts[2]
                 });
             }
             return groups;
         }
 
-        [Test, TestCaseSource("RandomGroupDataProvider")]
+
+        [Test, TestCaseSource("GroupDataFromFile")]
         public void GroupCreationTest(GroupData group)
         {
             //prepare
