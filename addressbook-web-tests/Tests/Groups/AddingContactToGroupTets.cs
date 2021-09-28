92caa205c086dc5aeea865e70dc457d8327ee57b
Boris <lbomarleyl@gmail.com>
2021-09-28 15:15:22 +0300

    improve test

--- addressbook-web-tests/Tests/Groups/AddingContactToGroupTets.cs
+++ addressbook-web-tests/Tests/Groups/AddingContactToGroupTets.cs
@@ -49,17 +49,41 @@ namespace WebAddressBookTests
         [Test]
         public void DeleteContactFromGroup()
         {
-            //prepare
+            //prepare group
+            List<GroupData> atLeastOneGroupExists = GroupData.GetAll();
+            if (atLeastOneGroupExists.Count == 0)
+            {
+                GroupData newGroup = new GroupData("New Group");
+                app.Groups.Create(newGroup);
+            }
             GroupData group = GroupData.GetAll()[0];
+
+            //prepare contact in group
+            List<ContactData> contactInGroup = group.GetContacts();
+            if (contactInGroup.Count == 0)
+            {
+                //prepare contact
+                List<ContactData> contactWithoutGroup = group.GetContacts();
+                if (contactWithoutGroup.Count == 0)
+                {
+                    ContactData newContact = new ContactData("New Contact");
+                    app.Contacts.Create(newContact);
+                }
+                ContactData contact = ContactData.GetAll().Except(group.GetContacts()).First();
+                app.Contacts.AddContactToGroup(contact, group);
+            }
+            
             List<ContactData> oldList = group.GetContacts();
-            ContactData contact = oldList[0];
+            ContactData contactToRemove = oldList[0];
 
             //action
-            app.Contacts.RemoveContactFromGroup(contact, group);
+            app.Contacts.RemoveContactFromGroup(contactToRemove, group);
 
             //validation
             List<ContactData> newList = group.GetContacts();
-            oldList.RemoveAt(0);                    
+            oldList.RemoveAt(0);
+            newList.Sort();
+            oldList.Sort();
             Assert.AreEqual(oldList, newList);
 
         }
